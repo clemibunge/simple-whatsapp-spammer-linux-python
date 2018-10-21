3f6668bd91ea880194b941fe7fdddb258422631f
clemibunge <clemibunge@gmail.com>
2018-10-21 20:35:13 +0200

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import subprocess
+import time
+import sys
+# open the textfile
+text = open(sys.argv[1]).read().strip()
+print("SPAM BEGINS IN 10 SECOUNDS!!!")
+time.sleep(10)
+for ch in text:
+    # type out the text
+    subprocess.call(["xdotool", "type", ch])
+    # increase or decrease the time below to type slower or faster
+    time.sleep(0.000000000000000000000000000000000000001)
